f7fe2b25536249962449e9298acb65b10b2e92c4
joelruiz2 <joelruiz2@gmail.com>
2020-01-05 04:11:15 -0600

    Updates

---
+++ precache-manifest.3a9a70e5db21a635b954165402cf3eee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e000332111838a07652d68f949723d",
+    "url": "/stream-racing-wheel/index.html"
+  },
+  {
+    "revision": "10460b39750f500a2cf8",
+    "url": "/stream-racing-wheel/static/js/2.d3a2e99b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stream-racing-wheel/static/js/2.d3a2e99b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a318a4f0b0fc20bce14e",
+    "url": "/stream-racing-wheel/static/js/main.aefca660.chunk.js"
+  },
+  {
+    "revision": "807370dca921577890e7",
+    "url": "/stream-racing-wheel/static/js/runtime-main.eaa6fdad.js"
+  }
+]);
